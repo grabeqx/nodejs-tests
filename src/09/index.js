85d73dcfa0dcbb4f6640b062f5e9909ffa9da6be
grabeqx <grabowski.kontakt@gmail.com>
2018-07-13 16:38:59 +0200

    Command line p.2

--- src/09/index.js
+++ src/09/index.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 const argv = require('./argv');
-
+const sortByDate = require('./utils').sortByDate;
+const newFileName = require('./utils').newFileName;
 
 if(!argv.validate(['dir', 'ext', 'format'])) {
     throw new Error('Nie podano poprawnych parametrów');
@@ -14,5 +15,21 @@ fs.readdir(dir, function(err, files) {
     if(err) {
         throw err;
     }
+
+    var validFiles = files.filter(function(file) {
+        return path.extname(file).slice(1) ===  argv.get('ext');
+    });
+
+    validFiles = sortByDate(validFiles, dir);
+
+    validFiles.forEach(function(file, index) {
+
+        try {
+            fs.renameSync(path.join(dir, file), path.join(dir, newFileName(argv.get('format'), argv.get('ext'), index + 1)));
+        } catch(e) {
+            throw new Error('Nie mozna zmienić');
+        }
+
+    });
 
 });
